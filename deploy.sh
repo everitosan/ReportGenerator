5ca019037481676cc390f337ad47be33a9cbefe9
Everardo Sánchez Hernández <everardo.ipn@gmail.com>
2017-12-25 20:42:29 -0600

    refact: add commit phase

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-## should be run as "git deploy [path]"
 if [ -z "$1" ]
 then
-  echo "Please specofy the dir of the dist project"
+  echo "Please specify the dir of the dist project"
+  exit 1
+elif [ -z "$2" ]
+then
+  echo "Please write the version of Deploy"
   exit 1
 fi
 npm run build
 sed -i -e 's/static\/img/ReportGenerator\/static\/img/g' ./$1/static/js/app.*.js
 sed -i -e 's/\/static/static/g' ./$1/index.html
+git add .
+git commit -m "Release V: $2"
 git subtree push --prefix $1 origin gh-pages
