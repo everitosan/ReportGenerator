e3af3da9d94458bad6dcc364ebdafe159170021b
Everardo Sánchez Hernández <everardo.ipn@gmail.com>
2017-12-25 18:21:26 -0600

    feat: update deploy

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ then
   echo "Please specofy the dir of the dist project"
   exit 1
 fi
-cat ./$1/index.html | sed -e 's/\/static/static/g' > ./$1/index.html
+sed -i -e 's/\/static/static/g' ./$1/index.html
 # git subtree push --prefix $1 origin gh-pages
